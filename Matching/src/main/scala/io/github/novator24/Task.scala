1e45d2eb471dd78934709091eee58a78bde7f261
musicnova <musicnova@yandex.ru>
2018-12-01 23:47:34 +0300

    0 up

--- Matching/src/main/scala/io/github/novator24/Task.scala
+++ Matching/src/main/scala/io/github/novator24/Task.scala
@@ -4,46 +4,172 @@ import java.io.File
 import com.github.tototoshi.csv._
 
 class Task {
-  case class Client(name: String
-                    , total_usd: Double
-                    , total_a_units: Double
-                    , total_b_units: Double
-                    , total_c_units: Double
-                    , total_d_units: Double) {}
+  // README https://github.com/tototoshi/scala-csv
+  implicit class StringImprovements(val s: String) {
+    import scala.util.control.Exception._
+    def toBigIntOpt = catching(classOf[NumberFormatException]) opt BigInt(s)
+    def toCharOpt = catching(classOf[NumberFormatException]) opt s(0)
+  }
 
-  def read_clients(filename: String): List[Client] = {
+  case class Client(name: String
+                    , total_usd: Option[BigInt]
+                    , total_a_units: Option[BigInt]
+                    , total_b_units: Option[BigInt]
+                    , total_c_units: Option[BigInt]
+                    , total_d_units: Option[BigInt]) {}
+
+  def readClients(filename: String): Stream[Client] = {
     implicit object ClientFormat extends DefaultCSVFormat {
       override val delimiter = '\t'
     }
     CSVReader.open(new File(filename)).toStream.map((x:List[String]) => Client(x.head
-      , x(1).toDouble, x(2).toDouble, x(3).toDouble, x(4).toDouble, x(5).toDouble)).toList
+      , x(1).toBigIntOpt
+      , x(2).toBigIntOpt
+      , x(3).toBigIntOpt
+      , x(4).toBigIntOpt
+      , x(5).toBigIntOpt))
   }
 
   case class Order(clientName: String
-                    , operation: Char
+                    , letter: Option[Char]
                     , emitName: String
-                    , unitPrice: BigInt
-                    , orderSize: BigInt) {}
-  def read_orders(filename: String): List[Order] = {
+                    , unitPrice: Option[BigInt]
+                    , orderSize: Option[BigInt]) {}
+  def readOrders(filename: String): Stream[Order] = {
     implicit object OrderFormat extends DefaultCSVFormat {
       override val delimiter = '\t'
     }
     CSVReader.open(new File(filename)).toStream.map((x:List[String]) => Order(x.head
-      , x(1)(0), x(2), BigInt(x(3)), BigInt(x(4)))).toList
+      , x(1).toCharOpt, x(2), x(3).toBigIntOpt, x(4).toBigIntOpt))
+  }
+
+  def checkClients(clients: Stream[Client]): Stream[Client] = {
+    clients.filter(_.name.nonEmpty)
+  }
+
+  def checkOrders(orders: Stream[Order]): Stream[Order] = {
+    orders.filter(_.clientName.nonEmpty)
+  }
+
+  case class Result(key: String, client: Option[Client], order: Option[Order]) {}
+  def calcClientsXOrders(clients: Stream[Client]
+                         , orders: Stream[Order]): Stream[Result] = {
+    val clientsMap = clients.groupBy(_.name)
+    val ordersMap = orders.groupBy(_.clientName)
+    clientsMap.keys.toStream.map(k => {
+      val c = clientsMap(k)
+      if (c.size > 1) throw new Exception("dup name "+k)
+      var res = c.head
+      for (o <- ordersMap(k)) {
+        if (o.letter == Option[Char]('s')) {
+          if(o.emitName == "A") {
+            res = Client(res.name
+              , Option[BigInt](res.total_usd.get + o.unitPrice.get * o.orderSize.get)
+              , Option[BigInt](res.total_a_units.get - o.orderSize.get)
+              , Option[BigInt](res.total_b_units.get)
+              , Option[BigInt](res.total_c_units.get)
+              , Option[BigInt](res.total_d_units.get)
+            )
+          }
+          if(o.emitName == "B") {
+            res = Client(res.name
+              , Option[BigInt](res.total_usd.get + o.unitPrice.get * o.orderSize.get)
+              , Option[BigInt](res.total_a_units.get)
+              , Option[BigInt](res.total_b_units.get - o.orderSize.get)
+              , Option[BigInt](res.total_c_units.get)
+              , Option[BigInt](res.total_d_units.get)
+            )
+          }
+          if(o.emitName == "C") {
+            res = Client(res.name
+              , Option[BigInt](res.total_usd.get + o.unitPrice.get * o.orderSize.get)
+              , Option[BigInt](res.total_a_units.get)
+              , Option[BigInt](res.total_b_units.get)
+              , Option[BigInt](res.total_c_units.get - o.orderSize.get)
+              , Option[BigInt](res.total_d_units.get)
+            )
+          }
+          if(o.emitName == "D") {
+            res = Client(res.name
+              , Option[BigInt](res.total_usd.get + o.unitPrice.get * o.orderSize.get)
+              , Option[BigInt](res.total_a_units.get)
+              , Option[BigInt](res.total_b_units.get)
+              , Option[BigInt](res.total_c_units.get)
+              , Option[BigInt](res.total_d_units.get - o.orderSize.get)
+            )
+          }
+        }
+        if (o.letter == Option[Char]('b')) {
+          if(o.emitName == "A") {
+            res = Client(res.name
+              , Option[BigInt](res.total_usd.get - o.unitPrice.get * o.orderSize.get)
+              , Option[BigInt](res.total_a_units.get + o.orderSize.get)
+              , Option[BigInt](res.total_b_units.get)
+              , Option[BigInt](res.total_c_units.get)
+              , Option[BigInt](res.total_d_units.get)
+            )
+          }
+          if(o.emitName == "B") {
+            res = Client(res.name
+              , Option[BigInt](res.total_usd.get - o.unitPrice.get * o.orderSize.get)
+              , Option[BigInt](res.total_a_units.get)
+              , Option[BigInt](res.total_b_units.get + o.orderSize.get)
+              , Option[BigInt](res.total_c_units.get)
+              , Option[BigInt](res.total_d_units.get)
+            )
+          }
+          if(o.emitName == "C") {
+            res = Client(res.name
+              , Option[BigInt](res.total_usd.get - o.unitPrice.get * o.orderSize.get)
+              , Option[BigInt](res.total_a_units.get)
+              , Option[BigInt](res.total_b_units.get)
+              , Option[BigInt](res.total_c_units.get + o.orderSize.get)
+              , Option[BigInt](res.total_d_units.get)
+            )
+          }
+          if(o.emitName == "D") {
+            res = Client(res.name
+              , Option[BigInt](res.total_usd.get - o.unitPrice.get * o.orderSize.get)
+              , Option[BigInt](res.total_a_units.get)
+              , Option[BigInt](res.total_b_units.get)
+              , Option[BigInt](res.total_c_units.get)
+              , Option[BigInt](res.total_d_units.get + o.orderSize.get)
+            )
+          }
+        }
+      }
+      Result(k, Option[Client](res), Option.empty[Order])
+    })
+  }
+
+  def saveResults(filename: String, results: Stream[Result]): Unit = {
+    // README https://github.com/tototoshi/scala-csv
+    implicit object OrderFormat extends DefaultCSVFormat {
+      override val delimiter = '\t'
+    }
+    val writer = CSVWriter.open(new File(filename))
+    results.foreach(r => {
+      writer.writeRow(List(r.client.get.name
+        , r.client.get.total_usd.get
+        , r.client.get.total_a_units.get
+        , r.client.get.total_b_units.get
+        , r.client.get.total_c_units.get
+        , r.client.get.total_d_units.get))
+    })
   }
 }
-
 
 object Task {
 
   def main(args: Array[String]): Unit = {
-    // README https://github.com/tototoshi/scala-csv
     val cls = new Task()
     val dir = "/home/user/GIT/7a_task/Matching"
-    val clients = cls.read_clients(new File(dir, "clients.txt").toString)
-    val orders = cls.read_orders(new File(dir, "orders.txt").toString)
-    print(clients.size)
-    print(orders.size)
+    val dataClients = cls.readClients(new File(dir, "clients.txt").toString)
+    val dataOrders = cls.readOrders(new File(dir, "orders.txt").toString)
+    val clients = cls.checkClients(dataClients)
+    val orders = cls.checkOrders(dataOrders)
+    val results = cls.calcClientsXOrders(clients, orders)
+    cls.saveResults(new File(dir, "results.txt").toString, results)
   }
 
 }
--- Matching/src/test/scala/io/github/novator24/TaskTest.scala
+++ Matching/src/test/scala/io/github/novator24/TaskTest.scala
@@ -1,9 +1,18 @@
 package io.github.novator24
 
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.{FlatSpec, PrivateMethodTester}
 
 // http://www.scalatest.org/user_guide/using_scalatest_with_sbt
-class TaskTest extends FlatSpec with Matchers {
+// https://stackoverflow.com/a/24375762
+class TaskTest extends FlatSpec with PrivateMethodTester {
 
+  "Clients" should "check correctly" in {
+    val cls = new Task()
+    val func = PrivateMethod[Task]('checkClients)
+    //val client = Client("", 0, 0, 0, 0, 0)
+    //val result = cls invokePrivate func(stream)
+    assert(true)
+  }
+  
 
 }
