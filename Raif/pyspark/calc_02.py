82aac1231a872d047c6ca63d4813f8d9a492a010
Guest YO <basovyo@mos.ru>
2018-12-07 01:35:35 +0300

    0 up

--- Raif/pyspark/calc_02.py
+++ Raif/pyspark/calc_02.py
@@ -7,8 +7,212 @@ from pyspark.sql import functions as f, SparkSession
 
 
 def algo(src, from_dt, to_dt):
+    res = steps(src, from_dt, to_dt)
+    return res
 
-    pass
+
+def steps(src, from_dt, to_dt):
+    import sys
+    MODULES_PATH = '../code/'
+    if MODULES_PATH not in sys.path:
+        sys.path.append(MODULES_PATH)
+    import mfuncs
+
+    import pandas as pd
+    import numpy as np
+    from tqdm import tqdm
+    tqdm.pandas()
+    pd.options.display.max_columns = 1000
+
+    import lightgbm as lgb
+
+    from sklearn.neighbors import NearestNeighbors
+
+    df_train = pd.read_csv('../data/train_2.csv')
+    df_test = pd.read_csv('../data/test_set.csv')
+    rnm = {
+        'atm_address_lat': 'atm_lat',
+        'atm_address_lon': 'atm_lon',
+        'pos_adress_lat': 'pos_lat',
+        'pos_adress_lon': 'pos_lon',
+        'home_add_lat': 'home_lat',
+        'home_add_lon': 'home_lon',
+        'work_add_lat': 'work_lat',
+        'work_add_lon': 'work_lon',
+    }
+    df_train.rename(columns=rnm, inplace=True)
+    df_test.rename(columns=rnm, inplace=True)
+
+    print(df_train.shape)
+
+    cid = df_train.sample(1)['customer_id'].values[0]
+    df_an = df_train[df_train.customer_id == cid]
+    df_point_dup = df_an.groupby(['pos_lat', 'pos_lon']).agg('size').reset_index()
+    df_point_dup.columns = ['pos_lat', 'pos_lon', 'pos_customer_freq']
+    df_an = pd.merge(df_an, df_point_dup, on=['pos_lat', 'pos_lon'], how='left')
+
+    df_an.head()
+
+    df_train.head()
+
+    df_train[df_train.type == 'pos'].drop_duplicates(['pos_lat',
+                                                      'pos_lon']).groupby(['terminal_id']).agg('size').value_counts()
+
+    df_train[df_train.type == 'atm'].drop_duplicates(['atm_lat',
+                                                      'atm_lon']).groupby(['terminal_id']).agg('size').value_counts()
+
+    df_train[df_train.terminal_id == '1e15d02895068c3a864432f0c06f5ece']['atm_address'].unique()
+
+    df_train[df_train.type == 'atm'].drop_duplicates(['atm_lat',
+                                                      'atm_lon']).groupby(['terminal_id']).agg('size')
+
+    import gmaps
+    API_KEY = 'AIzaSyCG_RL0_kavuEaJAqEN5xXbU4h0VJUbA9M'
+    gmaps.configure(api_key=API_KEY)  # Your Google API key
+
+    cid = '0dc0137d280a2a82d2dc89282450ff1b'
+    cid = df_train.sample(1)['customer_id'].values[0]
+    df_an = df_train[df_train.customer_id == cid]
+    center_home = df_an[['home_lat', 'home_lon']].drop_duplicates().values
+    center_work = df_an[['work_lat', 'work_lon']].drop_duplicates().values
+    points_pos = df_an[['pos_lat', 'pos_lon']].dropna().values
+    points_atm = df_an[['atm_lat', 'atm_lon']].dropna().values
+    print(center_home.shape, center_work.shape, points_pos.shape, points_atm.shape)
+
+    gmap = gmaps.Map()
+    if len(points_pos) > 0:
+        gmap.add_layer(gmaps.symbol_layer(points_pos, hover_text='pos',
+                                          fill_color="blue", stroke_color="blue", scale=3))
+    if len(points_atm) > 0:
+        gmap.add_layer(gmaps.symbol_layer(points_atm, hover_text='atm',
+                                          fill_color="red", stroke_color="red", scale=3))
+
+    if not np.isnan(center_home)[0][0]:
+        gmap.add_layer(gmaps.marker_layer(center_home, label='home'))
+    if not np.isnan(center_work)[0][0]:
+        gmap.add_layer(gmaps.marker_layer(center_work, label='work'))
+
+    gmap
+
+    # плотность работ
+    center_home = df_train[['home_lat', 'home_lon']].dropna().values
+    center_work = df_train[['work_lat', 'work_lon']].dropna().values
+
+    gmap = gmaps.Map()
+    gmap.add_layer(gmaps.symbol_layer(center_home, fill_color="red", stroke_color="red"))
+    gmap
+
+    np.isnan(center_home)
+
+    df_train.groupby(['customer_id']).agg('size').sort_values().value_counts()
+
+    df_test.customer_id.drop_duplicates().isin(df_train.customer_id.unique()).mean()
+
+    df_train['duplicated'] = df_train.duplicated()
+
+    df_point_dup = df_train.groupby(['customer_id', 'pos_lat', 'pos_lon']).agg('size').reset_index()
+    df_point_dup.columns = ['customer_id', 'pos_lat', 'pos_lon', 'pos_customer_freq']
+    df_train = pd.merge(df_train, df_point_dup, on=['customer_id', 'pos_lat', 'pos_lon'], how='left')
+
+    df_an
+
+    dfs = []
+    for cid in tqdm(df_train.customer_id.unique()):
+        df_an = df_train[df_train.customer_id == cid]
+        df_an = mfuncs.add_dist_to_neighbours(df_an)
+        dfs.append(df_an)
+
+    df_ = pd.concat(dfs)
+    df_.head()
+
+    df_.to_csv('../data/train_3.csv')
+
+    df_pos['transaction_date'] = pd.to_datetime(df_pos['transaction_date'], format='%Y-%m-%d')
+    df_pos['month'] = df_pos.transaction_date.dt.month
+    df_pos['day'] = df_pos.transaction_date.dt.day
+    df_pos['dayofyear'] = df_pos.transaction_date.dt.dayofyear
+    df_pos['dayofweek'] = df_pos.transaction_date.dt.dayofweek
+    df_pos.transaction_date.dtype
+
+    df_gb = df_pos.groupby('customer_id')
+    coord_stat_df = df_gb[['amount', 'pos_lat', 'pos_lon']].agg(['mean', 'max', 'min'])
+    coord_stat_df['transactions_per_user'] = df_gb.agg('size')
+    coord_stat_df.columns = ['_'.join(col).strip() for col in coord_stat_df.columns.values]
+    coord_stat_df.reset_index(inplace=True)
+    df_pos = pd.merge(df_pos, coord_stat_df, on='customer_id', how='left')
+
+    cols = ['pos_lat', 'pos_lon']
+    types = ['min', 'max', 'mean']
+    for c in cols:
+        for t in types:
+            df_pos['{}_diff_{}'.format(c, t)] = np.abs(df_pos[c] - df_pos['{}_{}'.format(c, t)])
+
+    df_pos = pd.concat([df_pos, pd.get_dummies(df_pos['mcc'], prefix='mcc')], axis=1)
+    del df_pos['mcc']
+
+    df_pos.head()
+
+    drop_cols = ['customer_id', 'terminal_id', 'target_home', 'target_work', 'atm_address',
+                 'pos_address', 'work_add_lat', 'work_add_lon', 'home_add_lat', 'home_add_lon',
+                 'city', 'type', 'transaction_date']
+    drop_cols += ['atm_address', 'atm_address_lat', 'atm_address_lon']
+    df_pos.drop(drop_cols, 1, errors='ignore').head()
+
+    from sklearn.model_selection import train_test_split, StratifiedKFold, KFold
+    df_pos_id = df_pos.customer_id.drop_duplicates().reset_index(drop=True)
+    skf_id = list(KFold(n_splits=5, shuffle=True, random_state=15).split(df_pos_id))
+    skf = []
+    for train_ind, test_ind in skf_id:
+        train_ind_ = df_pos[df_pos.customer_id.isin(df_pos_id.loc[train_ind].values)].index.values
+        test_ind_ = df_pos[df_pos.customer_id.isin(df_pos_id.loc[test_ind].values)].index.values
+        skf.append([train_ind_, test_ind_])
+
+    df_pos['target_work'].mean()
+
+    df_pos.head()
+
+    cid = '442fd7e3af4d8c3acd7807aa65bb5e85'
+    df_an = df_pos[df_pos.customer_id == cid]
+
+    df_an = mfuncs.add_dist_to_neighbours(df_an)
+
+    df_pos.customer_id.unique
+
+    if np.array([1]).size:
+        print(1)
+
+    lgb_train = lgb.Dataset(df_pos.drop(drop_cols, 1, errors='ignore'), df_pos['target_home'])
+
+    params = {
+        'objective': 'binary',
+        'num_leaves': 511,
+        'learning_rate': 0.05,
+        #     'metric' : 'error',
+        'feature_fraction': 0.8,
+        'bagging_fraction': 0.8,
+        'bagging_freq': 1,
+        'num_threads': 12,
+        'verbose': 0,
+    }
+
+    gbm = lgb.cv(params,
+                 lgb_train,
+                 num_boost_round=2000,
+                 folds=skf,
+                 verbose_eval=10,
+                 early_stopping_rounds=500)
+
+    df_pos.loc[i2].shape
+
+    i1, i2 = skf[0]
+    df_pos[df_pos.loc[i1]]['customer_id'].unique
+
+    df_pos.loc[i1]
+
+    df_pos.dtypes
+    res = df_pos
+    return res
+
 
 def update_last_partition(dst, from_dt, to_dt):
     prev_day = datetime.strptime(from_dt, '%Y-%m-%d') - timedelta(days=1)
