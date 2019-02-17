5b091f13bdfde5baa83b3f7a0f8a0da0c8046d3a
Guest YO <basovyo@mos.ru>
2019-02-17 21:01:56 +0300

    0 up

---
+++ Civ/src/main/scala/json/schema/pojo/Lancer.scala
@@ -0,0 +1,68 @@
+package json.schema.pojo
+
+import java.util.HashMap
+import java.util.List
+import java.util.Map
+import com.fasterxml.jackson.annotation.JsonAnyGetter
+import com.fasterxml.jackson.annotation.JsonAnySetter
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
+import scala.beans.{BeanProperty, BooleanBeanProperty}
+//remove if not needed
+import scala.collection.JavaConversions._
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder(Array("url", "name", "description", "productionCost", "combatStrength", "moves", "technology", "resource", "upgradesTo", "notes"))
+class Lancer {
+
+  @JsonProperty("url")
+  @BeanProperty
+  var url: String = _
+
+  @JsonProperty("name")
+  @BeanProperty
+  var name: String = _
+
+  @JsonProperty("description")
+  @BeanProperty
+  var description: String = _
+
+  @JsonProperty("productionCost")
+  @BeanProperty
+  var productionCost: java.lang.Integer = _
+
+  @JsonProperty("combatStrength")
+  @BeanProperty
+  var combatStrength: java.lang.Integer = _
+
+  @JsonProperty("moves")
+  @BeanProperty
+  var moves: java.lang.Integer = _
+
+  @JsonProperty("technology")
+  @BeanProperty
+  var technology: String = _
+
+  @JsonProperty("resource")
+  @BeanProperty
+  var resource: List[String] = null
+
+  @JsonProperty("upgradesTo")
+  @BeanProperty
+  var upgradesTo: List[String] = null
+
+  @JsonProperty("notes")
+  @BeanProperty
+  var notes: List[String] = null
+
+  @JsonIgnore
+  @BeanProperty
+  var additionalProperties: Map[String, Any] = new HashMap[String, Any]()
+
+  @JsonAnySetter
+  def setAdditionalProperty(name: String, value: AnyRef) {
+    this.additionalProperties.put(name, value)
+  }
+}
